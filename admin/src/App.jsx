addfff70defb89c02adaec5d88aed99ea52e0f58
mdraselmiah95 <russelroy15@gmail.com>
2024-08-19 11:02:15 +0600

    new project

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+import Chat from "./components/Chat";
+
+function App() {
+  return (
+    <>
+      <Chat />
+    </>
+  );
+}
+
+export default App;
